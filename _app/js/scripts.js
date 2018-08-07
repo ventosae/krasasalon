3cafff383a6a2ec5e5e15b3d010afd97ad2375ed
ventosae <anton.surov@hotmail.com>
2018-08-08 07:51:35 +1000

    ourwork section added

--- _app/js/scripts.js
+++ _app/js/scripts.js
@@ -10,3 +10,17 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+var elem = document.querySelector('.main-carousel1');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  prevNextButtons: false
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel1', {
+  // options
+});
