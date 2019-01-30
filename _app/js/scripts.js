fe349ccc5b6f75f462bb2fb76d4b297ce4f3018b
ventosae <anton.surov@hotmail.com>
2019-01-31 00:01:23 +1000

    our-works tablet done

--- _app/js/scripts.js
+++ _app/js/scripts.js
@@ -1,47 +1,44 @@
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   cellAlign: 'left',
-  contain:true,
-  watchCSS:true,
+  contain: true,
+  watchCSS: true,
   accessibility: false,
   imagesLoaded: false
 
 });
 
-var elem = document.querySelector('.main-carousel1');
-var flkty = new Flickity( elem, {
+var elem1 = document.querySelector('.main-carousel1');
+var flkty1 = new Flickity( elem1, {
   // options
   cellAlign: 'left',
   contain: true,
   prevNextButtons: false,
   contain: true,
-  watchCSS:true
+  watchCSS:false
 });
 
-// element argument can be a selector string
-//   for an individual element-
-var flkty = new Flickity( '.main-carousel1', {
-  // options
-});
+// var elem2 = document.querySelector('.main-carousel');
 
-var elem = document.querySelector('.main-carousel2');
-var flkty = new Flickity( elem, {
+// var flkty2 = new Flickity( elem2, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true,
+//   prevNextButtons: true,
+//   contain: true,
+//   watchCSS:true
+// });
+
+var elem2 = document.querySelector('.main-carousel2');
+var flkty2 = new Flickity( elem2, {
   cellAlign: 'left',
   contain: true,
   prevNextButtons: false,
   contain: true,
-  watchCSS:true
+  watchCSS:false
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel2', {
-  // options
-});
 
